255c2731aecf4331b44aca6389bc51cf5e17b581
Vladimir Orany <vladimir@orany.cz>
2018-10-13 07:57:14 +0200

    more simple base uri for http client

--- examples/heist/src/test/groovy/heist/HttpSpec.groovy
+++ examples/heist/src/test/groovy/heist/HttpSpec.groovy
@@ -7,9 +7,8 @@ import spock.lang.Specification
 
 class HttpSpec extends Specification{
 
-    @Rule Gru<Http> gru = Gru.equip(Http.steal(this)).prepare {                         // <1>
-        baseUri 'http://despicableme.wikia.com'                                         // <2>
-    }
+    @Rule Gru<Http> gru = Gru.equip(Http.steal(this))                                   // <1>
+                             .prepare('http://despicableme.wikia.com')                  // <2>
 
     void 'despicable me'() {
         expect:
