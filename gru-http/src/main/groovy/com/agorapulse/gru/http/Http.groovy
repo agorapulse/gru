a842945da1cb78eb03fa24ffea189be69d85b25f
Vladimir Orany <vladimir@orany.cz>
2020-03-12 09:41:42 +0100

    ability to customize OkHttpClient

--- gru-http/src/main/groovy/com/agorapulse/gru/http/Http.groovy
+++ gru-http/src/main/groovy/com/agorapulse/gru/http/Http.groovy
@@ -5,7 +5,12 @@ import com.agorapulse.gru.Client
 import com.agorapulse.gru.GruContext
 import com.agorapulse.gru.Squad
 import groovy.transform.CompileStatic
+import groovy.transform.stc.ClosureParams
+import groovy.transform.stc.SimpleType
 import okhttp3.OkHttpClient
+import space.jasan.support.groovy.closure.ConsumerWithDelegate
+
+import java.util.function.Consumer
 
 /**
  * Http Gru client preforms real HTTP calls against given URIs.
@@ -13,18 +18,37 @@ import okhttp3.OkHttpClient
 @CompileStatic
 class Http extends AbstractClient {
 
-    private final OkHttpClient httpClient = new OkHttpClient()
+    private final OkHttpClient httpClient
 
     private GruHttpRequest request
     private GruHttpResponse response
 
     static Http steal(Object unitTest) {
-        return new Http(unitTest)
+        return new Http(unitTest, null)
     }
 
-    private Http(Object unitTest) {
+    static Http steal(Object unitTest, Consumer<OkHttpClient.Builder> configuration) {
+        return new Http(unitTest, configuration)
+    }
+
+    static Http steal(
+        Object unitTest,
+        @DelegatesTo(value = OkHttpClient.Builder, strategy = Closure.DELEGATE_FIRST)
+        @ClosureParams(value = SimpleType, options = 'okhttp3.OkHttpClient.Builder')
+            Closure<OkHttpClient.Builder> configuration
+    ) {
+        return new Http(unitTest, ConsumerWithDelegate.create(configuration))
+    }
+
+    private Http(Object unitTest, Consumer<OkHttpClient.Builder> configuration) {
         super(unitTest)
         request = new GruHttpRequest()
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder()
+        if (configuration != null) {
+            configuration.accept(builder)
+        }
+        httpClient = builder.build()
     }
 
     @Override
